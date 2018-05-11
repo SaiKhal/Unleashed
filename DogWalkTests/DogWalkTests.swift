e5da1959df4e4c6b39a5b810bb574ab54175b00c
Masai Young <masai.young@gmail.com>
2018-05-11 16:05:11 -0400

    [Coordinator] Add Coordinator TestSpec

--- DogWalkTests/DogWalkTests.swift
+++ DogWalkTests/DogWalkTests.swift
@@ -7,30 +7,32 @@
 //
 
 import XCTest
+import Quick
+import Nimble
+
 @testable import DogWalk
 
-class DogWalkTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class CoordinatorSpec: QuickSpec {
+    override func spec() {
+        describe("A Coordinator") {
+            context("after being properly initialized") {
+                let coordinator: Coordinator = AppCoordinator()
+                
+                it("should have navigation controller") {
+                    expect(coordinator.navigationController).toNot(beNil())
+                }
+                
+                it("should have any child coordinators") {
+                    expect(coordinator.childCoordinators).toNot(beEmpty())
+                }
+                
+            }
+            
+            context("when you call start") {
+                it("should start the flow of a child coordinator") {
+                    expect(coordinator.start).to(equal("starting child coordinator"))
+                }
+            }
         }
     }
-    
 }
